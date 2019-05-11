f7893060c506949df1e22944283297184ecf0dfa
ariharananami <50040662+ariharananami@users.noreply.github.com>
2019-05-11 15:49:13 +0900

    first commit

---
+++ install.sh
@@ -0,0 +1,28 @@
+# このファイルのあるディレクトリをカレントディレクトリに
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+cd $SCRIPT_DIR
+
+# npmの確認
+# type npm
+# $? || {
+#     echo "npmが実行できません。Node.jsをインストールしてください"
+#     exit 1
+# }
+
+# http-serverのインストール
+echo
+npm ls -g --depth=0 | grep http-server
+$? || {
+    echo "http-serverをインストール"
+    npm install -g http-server
+}
+
+echo
+git clone https://github.com/ariharananami/ev3_scratch.git
+
+echo
+git clone https://github.com/ariharananami/scratchx.git
+
+echo
+echo "ふぇぇ"
+read
---
+++ launch.sh
@@ -0,0 +1,17 @@
+SCRIPT_DIR=$(cd $(dirname $0); pwd)         # このファイルのあるディレクトリ
+PATH_TO_IE="C:\Program Files\internet explorer\iexplore.exe"
+URL_TO_EXT="https://ariharananami.github.io/ev3_scratch/ev3_scratch.js"
+
+cd $SCRIPT_DIR
+
+echo
+echo "サーバを起動するよ"
+nohup http-server -p 80 &
+sleep 3
+
+echo
+echo "IEを開くよ"
+"$PATH_TO_IE" "http://localhost/scratchx/?url=$URL_TO_EXT"
+echo
+echo "ふぇぇ"
+read
